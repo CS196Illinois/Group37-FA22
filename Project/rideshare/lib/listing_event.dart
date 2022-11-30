48ede636f299cc42cec4a8e9195363f1ad37e313
Gabe Lazatin <18385233+gabelaza000@users.noreply.github.com>
2022-11-30 13:07:24 -0600

    Added listing_event class

--- Project/rideshare/lib/listing_event.dart
+++ Project/rideshare/lib/listing_event.dart
@@ -1,7 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
 class ListingEvent {
   final String? title;
+  final String? description;
   final DateTime? date;
   final String? number;
 
-  ListingEvent({this.title, this.date, this.number});
+  ListingEvent(
+    @required this.title,
+    @required this.description,
+    @required this.date,
+    @required this.number,
+  );
 }
